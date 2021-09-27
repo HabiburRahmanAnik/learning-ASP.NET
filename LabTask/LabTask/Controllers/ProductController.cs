f140e241b109fcdc7921e039cd8b96a2e170090a
habib264521@gmail.com <habib264521@gmail.com>
2021-09-27 14:16:06 +0600

    Lab task -- product

---
+++ LabTask/LabTask/Controllers/ProductController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LabTask.Models;
+using LabTask.Models.Entity;
+
+namespace LabTask.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            Database db = new Database();
+            var productList= db.Products.GetAllProducts();
+            return View(productList);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product p)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Products.AddProduct(p);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Update(int id)
+        {
+            Database db = new Database();
+           var p = db.Products.GetProductById(id);
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Product p,int id)
+        {
+            if (ModelState.IsValid)
+            {
+                Database db = new Database();
+                db.Products.UpdateProduct(p,id);
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Database db = new Database();
+            db.Products.deleteProduct(id);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ViewDetails(int id)
+        {
+            Database db = new Database();
+            var p = db.Products.GetProductById(id);
+            return View(p);
+        }
+
+    }
+}
---
+++ LabTask/LabTask/Models/Entity/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabTask.Models.Entity
+{
+    public class Product
+    {
+        public int Id { set; get; }
+        [Required(ErrorMessage ="Product name can't be Empty")]
+        public string ProductName { get; set; }
+        [Required(ErrorMessage ="Price can't be empty")]
+        public float Price { get; set; }
+        [Required(ErrorMessage ="Quantitycan't be empty")]
+        public int Quantity { get; set; }
+        [Required(ErrorMessage ="Description can't be empty")]
+        public string Description { get; set; }
+    }
+}
