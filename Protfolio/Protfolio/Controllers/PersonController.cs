3c6d5751982dfc0b7c1fa6d3954378c77c1de935
HabiburRahmanAnik <habib264521@gmail.com>
2021-10-02 15:16:45 +0600

    add Protfolio

---
+++ Protfolio/Protfolio/Controllers/PersonController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Protfolio.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Bio()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Reffrence()
+        {
+            return View();
+        }
+    }
+}
