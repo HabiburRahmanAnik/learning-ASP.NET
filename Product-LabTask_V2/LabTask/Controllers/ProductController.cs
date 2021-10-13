4855754dad6b4986d85d74c9c7e18716866cc8c1
HabiburRahmanAnik <habib264521@gmail.com>
2021-10-13 23:58:25 +0600

    complete cancel order

--- Product-LabTask_V2/LabTask/Controllers/ProductController.cs
+++ Product-LabTask_V2/LabTask/Controllers/ProductController.cs
@@ -66,7 +66,7 @@ namespace LabTask.Controllers
             db.Products.deleteProduct(id);
             return RedirectToAction("Index");
         }
-
+        [AllowAnonymous]
         public ActionResult ViewDetails(int id)
         {
             Database db = new Database();
